498db87ff28db94495a69c0f23fce988171edd1c
theiereman <thomas.eyermann@protonmail.com>
2025-03-31 20:35:33 +0200

    feat(recipes): added recipe average rating

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,4 +1,6 @@
 class Rating < ApplicationRecord
+  validates :score, presence: true, inclusion: { in: 1..5 }
+
   belongs_to :user
   belongs_to :recipe
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,8 @@
 class Recipe < ApplicationRecord
+  has_many :ratings, dependent: :destroy
+  has_many :ingredients
+
+  def average_rating
+    ratings.average(:score).to_f
+  end
 end
