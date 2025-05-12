6672fa284fc65a52e2f975950bd624843dc0c732
theiereman <thomas.eyermann@protonmail.com>
2025-05-12 18:46:24 +0200

    added action mailer to notify users when recipes are commented or rated

---
+++ app/mailers/notification_mailer.rb
@@ -0,0 +1,37 @@
+class NotificationMailer < ApplicationMailer
+  default from: "contact@dotsncircles.com"
+
+  def test(email)
+    mail(to: email, subject: "Hello World!")
+  end
+
+  # Envoie un email quand un commentaire est ajouté à une recette
+  def new_comment_notification
+    @comment = params[:comment]
+    @recipe = @comment.recipe
+    @recipe_owner = @recipe.user
+    @commenter = @comment.user
+
+    # return if @recipe_owner.id == @commenter.id # Ne pas notifier si l'utilisateur commente sa propre recette
+
+    mail(
+      to: @recipe_owner.email,
+      subject: "Nouveau commentaire sur votre recette \"#{@recipe.name}\""
+    )
+  end
+
+  # Envoie un email quand une note est ajoutée à une recette
+  def new_rating_notification
+    @rating = params[:rating]
+    @recipe = @rating.recipe
+    @recipe_owner = @recipe.user
+    @rater = @rating.user
+
+    # return if @recipe_owner.id == @rater.id # Ne pas notifier si l'utilisateur note sa propre recette
+
+    mail(
+      to: @recipe_owner.email,
+      subject: "Nouvelle note sur votre recette \"#{@recipe.name}\""
+    )
+  end
+end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,16 +58,20 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "example.com" }
+  config.action_mailer.default_url_options = { host: "ratemymouth.dotsncircles.com" }
+
+  # Configure email delivery method
+  config.action_mailer.delivery_method = :smtp
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via rails credentials:edit.
-  # config.action_mailer.smtp_settings = {
-  #   user_name: Rails.application.credentials.dig(:smtp, :user_name),
-  #   password: Rails.application.credentials.dig(:smtp, :password),
-  #   address: "smtp.example.com",
-  #   port: 587,
-  #   authentication: :plain
-  # }
+  config.action_mailer.smtp_settings = {
+    user_name: Rails.application.credentials.dig(:smtp, :user_name),
+    password: Rails.application.credentials.dig(:smtp, :password),
+    address: Rails.application.credentials.dig(:smtp, :address),
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
