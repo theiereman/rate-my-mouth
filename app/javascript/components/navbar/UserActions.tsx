ffc9a0a16382fe55ffa79bfca2c7de0bca2a4942
theiereman <thomas.eyermann@protonmail.com>
2025-05-14 12:10:18 +0200

    feat: open navbar items on hover instead of click

--- app/javascript/components/navbar/UserActions.tsx
+++ app/javascript/components/navbar/UserActions.tsx
@@ -1,66 +1,37 @@
 import { Link, usePage } from "@inertiajs/react";
 import { PageProps } from "../../types";
 import ProfilePicPlaceholder from "../ProfilePicPlaceholder";
-import { useState, useRef, useEffect } from "react";
 
 export default function UserActions() {
   const { current_user } = usePage<PageProps>().props;
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  const dropdownRef = useRef<HTMLDivElement>(null);
-
-  // Close dropdown when clicking outside
-  useEffect(() => {
-    function handleClickOutside(event: MouseEvent) {
-      if (
-        dropdownRef.current &&
-        !dropdownRef.current.contains(event.target as Node)
-      ) {
-        setIsDropdownOpen(false);
-      }
-    }
-
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [dropdownRef]);
 
   return (
-    <div className="relative" ref={dropdownRef}>
-      <button
-        className="flex items-center space-x-3 focus:outline-none cursor-pointer"
-        onClick={() => setIsDropdownOpen(!isDropdownOpen)}
-      >
+    <div className="relative group">
+      <button className="flex items-center space-x-3 focus:outline-none cursor-pointer ">
         <div className="hidden sm:block text-right">
           <p className="text-sm font-medium text-neutral-700">
             {current_user.username}
           </p>
           <p className="text-xs text-neutral-500">{current_user.email}</p>
         </div>
-        <ProfilePicPlaceholder className="transition-transform hover:scale-105" />
+        <ProfilePicPlaceholder className="transition-transform group-hover:scale-105" />
       </button>
 
-      {/* Dropdown menu */}
-      {isDropdownOpen && (
-        <div className="absolute right-0 mt-2 bg-white rounded-md shadow-lg py-1 z-10">
-          {/* User actions */}
-          <Link
-            href="/my_profile"
-            className="block w-full px-4 text-end py-2 text-sm text-neutral-600 hover:bg-neutral-100 transition-colors"
-            onClick={() => setIsDropdownOpen(false)}
-          >
-            Mon profil
-          </Link>
-          <Link
-            href="/users/sign_out"
-            method="delete"
-            className="block w-full text-end px-4 py-2 text-sm text-red-600 hover:bg-neutral-100 transition-colors"
-            onClick={() => setIsDropdownOpen(false)}
-          >
-            Se déconnecter
-          </Link>
-        </div>
-      )}
+      <div className="hidden group-hover:block absolute right-0 bg-white rounded-md shadow-lg py-1 z-10">
+        <Link
+          href="/my_profile"
+          className="block w-full px-4 text-end py-2 text-sm text-neutral-600 hover:bg-neutral-100 transition-colors"
+        >
+          Mon profil
+        </Link>
+        <Link
+          href="/users/sign_out"
+          method="delete"
+          className="block w-full text-end px-4 py-2 text-sm text-red-600 hover:bg-neutral-100 transition-colors"
+        >
+          Se déconnecter
+        </Link>
+      </div>
     </div>
   );
 }
--- app/javascript/components/theme/ThemeSelector.tsx
+++ app/javascript/components/theme/ThemeSelector.tsx
@@ -1,53 +1,27 @@
-import { useEffect, useRef, useState } from "react";
 import CustomThemeSelector from "./CustomThemeSelector";
 import PredefinedThemeSelector from "./PredefinedThemeSelector";
 
 export default function ThemeSelector() {
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  const dropdownRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    function handleClickOutside(event: MouseEvent) {
-      if (
-        dropdownRef.current &&
-        !dropdownRef.current.contains(event.target as Node)
-      ) {
-        setIsDropdownOpen(false);
-      }
-    }
-
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [dropdownRef]);
-
   return (
-    <div className="relative" ref={dropdownRef}>
-      <button
-        className="w-10 h-10 rounded-full bg-primary-100 flex items-center justify-center space-x-3 focus:outline-none cursor-pointer transition-transform hover:scale-105"
-        onClick={() => setIsDropdownOpen(!isDropdownOpen)}
-      >
+    <div className="relative group">
+      <button className="w-10 h-10 rounded-full bg-primary-100 flex items-center justify-center space-x-3 focus:outline-none cursor-pointer transition-transform group-hover:scale-105">
         <span className="material-symbols-outlined text-primary-600">
           color_lens
         </span>
       </button>
 
-      {/* Dropdown menu */}
-      {isDropdownOpen && (
-        <div className="absolute right-0 mt-2 w-64 bg-white rounded-md shadow-lg z-10 py-2 px-4 flex flex-col gap-2">
-          <div id="predefined-colors">
-            <h1 className="text-sm font-medium text-neutral-700 mb-2">
-              Thème de l'application
-            </h1>
-            <PredefinedThemeSelector />
-          </div>
-
-          <div id="custom-colors">
-            <CustomThemeSelector />
-          </div>
+      <div className="hidden group-hover:flex absolute right-0 w-64 bg-white rounded-md shadow-lg z-10 py-2 px-4 flex-col gap-2">
+        <div id="predefined-colors">
+          <h1 className="text-sm font-medium text-neutral-700 mb-2">
+            Thème de l'application
+          </h1>
+          <PredefinedThemeSelector />
         </div>
-      )}
+
+        <div id="custom-colors">
+          <CustomThemeSelector />
+        </div>
+      </div>
     </div>
   );
 }
