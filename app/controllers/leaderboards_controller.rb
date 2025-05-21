6f48a9fb06c9f8995cc62f329bed337e785a1415
theiereman <thomas.eyermann@protonmail.com>
2025-05-21 08:13:02 +0200

    feat: added more leaderboard types

--- app/controllers/leaderboards_controller.rb
+++ app/controllers/leaderboards_controller.rb
@@ -1,11 +1,21 @@
 class LeaderboardsController < ApplicationController
   def show
-    @users = User.left_joins(:recipes).group(:id).all.order("COUNT(recipes.id) DESC").limit(10)
+    case params[:type]
+    when "recipes"
+      @users = User.left_joins(:recipes).group(:id).all.order("COUNT(recipes.id) DESC").limit(10)
+    when "comments"
+      @users = User.left_joins(:comments).group(:id).all.order("COUNT(comments.id) DESC").limit(10)
+    when "ratings"
+      @users = User.left_joins(:ratings).group(:id).all.order("COUNT(ratings.id) DESC").limit(10)
+    else
+      @users = User.left_joins(:recipes).group(:id).all.order("COUNT(recipes.id) DESC").limit(10)
+    end
 
     render inertia: "Leaderboard/Show", props: {
       users: @users.map do |user|
-        user.as_json(only: [ :id, :username ], methods: [ :number_of_recipes, :avatar_url ])
-      end
+        user.as_json(only: [ :id, :username ], methods: [ :number_of_comments, :number_of_ratings, :number_of_recipes, :avatar_url ])
+      end,
+      type: params[:type] || "recipes"
     }
   end
 end
--- app/javascript/pages/Leaderboard/Show.tsx
+++ app/javascript/pages/Leaderboard/Show.tsx
@@ -1,34 +1,77 @@
 import { UserType } from "@customTypes/user.types";
-import { Card, Badge } from "@components/ui";
+import { Card, Badge, LinkButton } from "@components/ui";
 import UserAvatar from "@components/Users/UserAvatar";
 import UserLink from "@components/Users/UserLink";
 import Page from "@layouts/Page";
+import { lowerCase } from "lodash";
+import { LeaderboardProps, leaderboardTypes } from "./Show.props";
 
-interface LeaderboardProps {
-  users: UserType[];
-}
+const getMedalColor = (index: number) => {
+  switch (index) {
+    case 0:
+      return "text-yellow-500"; // Gold
+    case 1:
+      return "text-gray-400"; // Silver
+    case 2:
+      return "text-amber-700"; // Bronze
+    default:
+      return "text-neutral-500";
+  }
+};
 
-export default function Show({ users }: LeaderboardProps) {
-  // Get medal color based on position
-  const getMedalColor = (index: number) => {
-    switch (index) {
-      case 0:
-        return "text-yellow-500"; // Gold
-      case 1:
-        return "text-gray-400"; // Silver
-      case 2:
-        return "text-amber-700"; // Bronze
+const getMedalIcon = (index: number) => {
+  switch (index) {
+    case 0:
+      return "emoji_events";
+    case 1:
+      return "military_tech";
+    case 2:
+      return "workspace_premium";
+    default:
+      return "";
+  }
+};
+
+export default function Show({ users, type = "recipes" }: LeaderboardProps) {
+  const getBadgeValue = (user: UserType) => {
+    switch (type) {
+      case "recipes":
+        return user.number_of_recipes;
+      case "comments":
+        return user.number_of_comments;
+      case "ratings":
+        return user.number_of_ratings;
       default:
-        return "text-neutral-500";
+        return user.number_of_recipes;
     }
   };
+
+  const typeTitle = lowerCase(
+    leaderboardTypes.find((t) => t.value === type)?.title || ""
+  );
 
   return (
     <Page
       title="Classement"
-      subtitle="Classement des utilisateurs par nombre de recettes postées"
+      subtitle={`Classement des utilisateurs par nombre de ${typeTitle} postées`}
     >
+      {leaderboardTypes.map((t) => (
+        <LinkButton
+          variant="ghost"
+          preserveScroll
+          key={t.value}
+          href={`/leaderboard?type=${t.value}`}
+          className={`px-4 py-2 rounded-none rounded-t-md! hover:text-primary-600! ${
+            type === t.value
+              ? "bg-primary-100! text-primary-600"
+              : "text-neutral-600"
+          }`}
+        >
+          {t.title}
+        </LinkButton>
+      ))}
       <Card className="p-0!">
+        <div></div>
         {users.length > 0 ? (
           <ul className="divide-y divide-neutral-200">
             {users.map((user, index) => (
@@ -39,20 +82,14 @@ export default function Show({ users }: LeaderboardProps) {
                   )}`}
                 >
                   {index + 1}
-                  {index < 3 && (
-                    <span className="material-symbols-outlined block">
-                      {index === 0
-                        ? "emoji_events"
-                        : index === 1
-                        ? "military_tech"
-                        : "workspace_premium"}
-                    </span>
-                  )}
+                  <span className="material-symbols-outlined block">
+                    {getMedalIcon(index)}
+                  </span>
                 </span>
                 <UserAvatar user={user} size="md" className="flex-shrink-0" />
                 <UserLink className="flex-1" user={user} />
                 <Badge variant="primary" size="md">
-                  {user.number_of_recipes} recettes
+                  {`${getBadgeValue(user)} ${typeTitle}`}
                 </Badge>
               </li>
             ))}
@@ -60,7 +97,7 @@ export default function Show({ users }: LeaderboardProps) {
         ) : (
           <div className="text-center py-8">
             <p className="text-neutral-600">
-              Aucun utilisateur n'a encore posté de recettes
+              Aucun utilisateur n'a encore posté de {typeTitle}
             </p>
           </div>
         )}
