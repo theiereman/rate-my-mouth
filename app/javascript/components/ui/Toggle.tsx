1d53628d9c7c8d075786150ec0b365a6b7ceab39
theiereman <thomas.eyermann@protonmail.com>
2025-05-23 18:13:23 +0200

    style: updated toggle style

--- app/javascript/components/ui/Toggle.tsx
+++ app/javascript/components/ui/Toggle.tsx
@@ -17,21 +17,26 @@ export default function Toggle({
   ...props
 }: ToggleProps) {
   return (
-    <label className="flex flex-col cursor-pointer">
-      <div className="flex items-center">
+    <div className="flex items-center gap-4">
+      <label className="relative inline-block items-center h-8 w-14 cursor-pointer rounded-full bg-gray-300 transition [-webkit-tap-highlight-color:_transparent] has-[:checked]:bg-primary-600">
         <input
           {...props}
           checked={checked}
           onChange={onChange}
+          className="peer sr-only"
           type="checkbox"
-          className="sr-only peer"
         />
-        <div className="relative w-11 h-6 bg-neutral-200 peer-focus:outline-none peer-focus:ring-2 peer-focus:focus:ring-primary-500 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[3px] after:bg-white after:border-gray-300 after:border after:rounded-full after:size-[1.2em] after:transition-all peer-checked:bg-primary-600"></div>
-        <span className="ms-3 text-sm text-neutral-800">{label}</span>
+        <span
+          id="toggle"
+          className="absolute inset-y-0 start-0 m-1 size-6 rounded-full bg-gray-300 ring-[6px] ring-inset ring-white transition-all peer-checked:start-8 peer-checked:w-2 peer-checked:bg-white peer-checked:ring-transparent"
+        ></span>
+      </label>
+      <div className="flex flex-col">
+        <span>{label}</span>
+        {helperText && (
+          <span className="text-xs text-neutral-500 mt-1">{helperText}</span>
+        )}
       </div>
-      {helperText && (
-        <span className="text-xs text-neutral-500 mt-1">{helperText}</span>
-      )}
-    </label>
+    </div>
   );
 }
