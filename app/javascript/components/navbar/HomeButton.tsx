b0bdc2364139e9f15ae4c197f17c619be7e69748
theiereman <thomas.eyermann@protonmail.com>
2025-05-31 12:21:07 +0200

    use of the logo for the home button

--- app/javascript/components/navbar/HomeButton.tsx
+++ app/javascript/components/navbar/HomeButton.tsx
@@ -1,4 +1,5 @@
 import { Link } from "@inertiajs/react";
+import logo from "../../assets/images/logo.svg";
 
 export default function HomeButton({ className }: { className?: string }) {
   return (
@@ -6,10 +7,7 @@ export default function HomeButton({ className }: { className?: string }) {
       href="/"
       className={`flex items-center space-x-2 text-primary-600 font-bold text-xl transition-transform hover:scale-105 ${className}`}
     >
-      <span className="material-symbols-outlined text-primary-600">
-        chef_hat
-      </span>
-      <span>RateMyMouth</span>
+      <img src={logo} className="size-14" />
     </Link>
   );
 }
