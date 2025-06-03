e893227880d12e12586eb72d853312d1cf6be778
theiereman <thomas.eyermann@protonmail.com>
2025-06-03 15:08:36 +0200

    toggle shitty display fix

--- app/javascript/components/ui/Toggle.tsx
+++ app/javascript/components/ui/Toggle.tsx
@@ -9,28 +9,19 @@ interface ToggleProps extends InputHTMLAttributes<HTMLInputElement> {
 }
 
 export default function Toggle({
-  checked,
   label,
   helperText,
-  disabled,
   onChange,
   ...props
 }: ToggleProps) {
   return (
     <div className="flex items-center gap-4">
-      <label className="relative inline-block items-center h-8 w-14 cursor-pointer rounded-full bg-gray-300 transition [-webkit-tap-highlight-color:_transparent] has-[:checked]:bg-primary-600">
-        <input
-          {...props}
-          checked={checked}
-          onChange={onChange}
-          className="peer sr-only"
-          type="checkbox"
-        />
-        <span
-          id="toggle"
-          className="absolute inset-y-0 start-0 m-1 size-6 rounded-full bg-gray-300 ring-[6px] ring-inset ring-white transition-all peer-checked:start-8 peer-checked:w-2 peer-checked:bg-white peer-checked:ring-transparent"
-        ></span>
-      </label>
+      <input
+        {...props}
+        onChange={onChange}
+        className="rounded border-neutral-300 text-primary-600 focus:ring-primary-500 disabled:opacity-50 cursor-pointer"
+        type="checkbox"
+      />
       <div className="flex flex-col">
         <span>{label}</span>
         {helperText && (
