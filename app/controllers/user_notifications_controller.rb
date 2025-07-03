a12fde93f3c4c849517d7359fbcef00b71654fd0
theiereman <thomas.eyermann@protonmail.com>
2025-07-03 14:29:14 +0200

    notification specifi controller

---
+++ app/controllers/user_notifications_controller.rb
@@ -0,0 +1,34 @@
+class UserNotificationsController < ApplicationController
+  include Paginatable
+
+  def index
+    @notifications = current_user.notifications.order(created_at: :desc)
+    @pagy, @notifications = paginate_collection(@notifications)
+    @presented_notifications = @notifications.map { |notification| NotificationPresenter.new(notification).to_h }
+
+    render json: {
+      notifications: @presented_notifications,
+      pagy: pagy_metadata(@pagy)
+    }
+  end
+
+  def mark_as_read
+    notification_ids = params[:notification_ids]
+    notifications = current_user.notifications.where(id: notification_ids)
+
+    return if notifications.empty?
+
+    notifications.each(&:mark_as_read!)
+
+    redirect_back_or_to root_path
+  rescue => e
+    render json: { success: false, message: e.message }, status: :unprocessable_entity
+  end
+
+  def mark_all_as_read
+    current_user.notifications.mark_all_as_read!
+    render json: { success: true, message: "All notifications marked as read" }
+  rescue => e
+    render json: { success: false, message: e.message }, status: :unprocessable_entity
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,9 @@ Rails.application.routes.draw do
   patch "select_achievement_as_title", to: "achievements#select_achievement_as_title", as: :select_achievement_as_title
 
   get "my_profile", to: "users#my_profile", as: :my_profile
-  get "my_notifications", to: "users#my_notifications", as: :my_notifications
+
+  get "notifications", to: "user_notifications#index", as: :notifications
+  post "notifications/mark_as_read", to: "user_notifications#mark_as_read", as: :mark_notifications_as_read
 
   resources :recipes, concerns: [ :commentable ] do
     resources :ratings, only: [ :create ]
