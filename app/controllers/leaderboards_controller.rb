f1ca689f763a41675c57bf73fc616438a743bc4b
theiereman <141121240+theiereman@users.noreply.github.com>
2025-07-09 16:58:39 +0200

    chore: n+1 queries fix using bullet gem (#38)
    * fix: recipes missing info when retrieving in json format
    
    * added bullet gem
    
    * fixed n+1 queries issues
    
    * fix: tests not passing becaues of n+1 issue and bullet config issues

--- app/controllers/leaderboards_controller.rb
+++ app/controllers/leaderboards_controller.rb
@@ -2,13 +2,13 @@ class LeaderboardsController < ApplicationController
   def show
     case params[:type]
     when "recipes"
-      @users = User.left_joins(:recipes).group(:id).all.order("COUNT(recipes.id) DESC").limit(10)
+      @users = User.includes(:avatar_attachment).left_joins(:recipes).group(:id).all.order("COUNT(recipes.id) DESC").limit(10)
     when "comments"
-      @users = User.left_joins(:comments).group(:id).all.order("COUNT(comments.id) DESC").limit(10)
+      @users = User.includes(:avatar_attachment).left_joins(:comments).group(:id).all.order("COUNT(comments.id) DESC").limit(10)
     when "ratings"
-      @users = User.left_joins(:ratings).group(:id).all.order("COUNT(ratings.id) DESC").limit(10)
+      @users = User.includes(:avatar_attachment).left_joins(:ratings).group(:id).all.order("COUNT(ratings.id) DESC").limit(10)
     else
-      @users = User.left_joins(:recipes).group(:id).all.order("COUNT(recipes.id) DESC").limit(10)
+      @users = User.includes(:avatar_attachment).left_joins(:recipes).group(:id).all.order("COUNT(recipes.id) DESC").limit(10)
     end
 
     render inertia: "Leaderboard/Show", props: {
