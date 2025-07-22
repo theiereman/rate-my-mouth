8c19893103d9b77fe4f139059f2252411d41bdbf
theiereman <thomas.eyermann@protonmail.com>
2025-07-22 12:06:37 +0200

    single select combo

--- app/javascript/components/ui/v2/Combo.tsx
+++ app/javascript/components/ui/v2/Combo.tsx
@@ -1,56 +1,86 @@
 import { Input } from "@components/ui";
-import { InputHTMLAttributes, useState } from "react";
+import { InputHTMLAttributes, useEffect, useRef, useState } from "react";
 
 interface ComboValue {
-  value: string;
+  value: any;
   label: string;
 }
 
 type ComboProps = InputHTMLAttributes<HTMLInputElement> & {
+  selectedValue?: number;
+  selectedValues?: number[]; //multi select
   values: ComboValue[];
   label: string;
-  onSelectedValue?: (value: string) => void;
+  onSelectedValue?: (value: ComboValue | null) => void;
   onSearchValueChange?: (value: string) => void;
 };
 
 export default function Combo({
+  selectedValue,
   values = [],
   onSelectedValue,
   onSearchValueChange,
   ...props
 }: ComboProps) {
-  const [isOpen, setIsOpen] = useState(true);
-  const [searchValue, setSearchValue] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+
+  const divRef = useRef<HTMLDivElement>(null);
+
+  //update input value on value change
+  useEffect(() => {
+    const found = values.find((v) => v.value === selectedValue);
+    if (!inputValue) setInputValue(found ? found.label : "");
+  }, [selectedValue, values]);
+
+  //handle click outside the div
+  useEffect(() => {
+    const handleClick = (event: MouseEvent) => {
+      if (!divRef.current) return;
+
+      if (!divRef.current.contains(event.target as Node)) {
+        setIsOpen(false);
+        return;
+      }
+    };
+    document.addEventListener("mousedown", handleClick);
+    return () => {
+      document.removeEventListener("mousedown", handleClick);
+    };
+  }, []);
 
   const handleSearchValueChange = (value: string) => {
-    setSearchValue(value);
+    setInputValue(value);
+    if (!value && selectedValue) {
+      onSelectedValue && onSelectedValue(null);
+    }
     onSearchValueChange && onSearchValueChange(value);
   };
 
-  const handleSelectedValueChange = (value: string) => {
+  const handleSelectedValueChange = (value: ComboValue) => {
     onSelectedValue && onSelectedValue(value);
-    setSearchValue(value);
+    setInputValue(value.label);
     setIsOpen(false);
   };
 
   return (
-    <div className="relative">
+    <div ref={divRef} className="relative">
       <Input
         label={props.label}
-        value={searchValue}
+        value={inputValue}
         onChange={(e) => handleSearchValueChange(e.target.value)}
         onClick={() => setIsOpen(true)}
-        className={isOpen ? "border-b-0" : ""}
+        className={isOpen ? "border-b-0" : undefined}
       />
       {isOpen && (
-        <div className="border-primary-900 absolute z-10 max-h-60 w-full overflow-auto border-x-3 border-t-1 border-b-3 bg-white shadow-lg">
+        <div className="border-primary-900 absolute z-10 max-h-60 w-full overflow-auto border-x-3 border-b-3 bg-white shadow-lg">
           {values.length > 0 ? (
             <ul>
               {values.map((value, index) => (
                 <li
                   key={index}
                   className="hover:bg-primary-900 hover:text-background text-primary-900 cursor-pointer px-4 py-2 transition-all hover:font-bold"
-                  onClick={() => handleSelectedValueChange(value.value)}
+                  onClick={() => handleSelectedValueChange(value)}
                 >
                   {value.label}
                 </li>
