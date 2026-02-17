96013613aa3db3e087cdc3b914f22afabe8c22a9
theiereman <thomas.eyermann@protonmail.com>
2026-02-17 17:27:45 +0100

    feat(layout): made it bigger

--- app/javascript/layouts/AuthenticatedLayout.tsx
+++ app/javascript/layouts/AuthenticatedLayout.tsx
@@ -26,7 +26,7 @@ export default function AuthenticatedLayout({
       >
         <Navbar />
       </div>
-      <div className="mx-auto max-w-4xl px-4 py-8 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-6xl px-4 py-8 sm:px-6 lg:px-8">
         {children}
       </div>
     </BaseLayout>
